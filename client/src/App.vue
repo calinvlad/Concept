43d2d5d3a489edd5668f54b4d94b453ada49903b
calinvlad <30374710+calinvlad@users.noreply.github.com>
2019-05-31 08:54:19 +0300

    Vue routes setup

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+<!--    <Footer />-->
+  </div>
+</template>
+
+<script>
+  import Header from '@/layouts/Header'
+  import Footer from '@/layouts/Footer'
+
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
---
+++ client/src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    { path: '*', redirect: '/products' },
+    {
+      path: '/auth/login',
+      name: 'login',
+      component: () => import('./pages/Auth/Login')
+    },
+    {
+      path: '/auth/register',
+      name: 'register',
+      component: () => import('./pages/Auth/Register')
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('./pages/Products')
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: () => import('./pages/Checkout')
+    }
+  ]
+})
